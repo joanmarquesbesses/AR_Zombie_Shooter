d7f3996365961b95d83ab69907d342fc4400aef0
Marc Avante <avantecaballe@gmail.com>
2025-03-25 12:45:34 +0100

    Shooting started

--- ArMobile-main/Assets/Scripts/GameManager.cs
+++ ArMobile-main/Assets/Scripts/GameManager.cs
@@ -1,6 +1,9 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
+using UnityEngine.XR.ARFoundation;
+using UnityEngine.XR.ARSubsystems;
 
 public class GameManager : MonoBehaviour
 {
@@ -8,9 +11,11 @@ public class GameManager : MonoBehaviour
     private GameObject[] anchors;
     public GameObject zombi;
     private bool spawn = true;
+    private GameObject camera;
+    public LayerMask enemyLayer;
     void Start()
     {
-        
+        camera = GameObject.FindWithTag("MainCamera");
     }
 
     // Update is called once per frame
@@ -25,6 +30,34 @@ public class GameManager : MonoBehaviour
                 Instantiate(zombi, anchors[1].transform);
                 Instantiate(zombi, anchors[2].transform);
                 spawn = false;
+            }
+        }
+
+        if (Pointer.current != null && Pointer.current.press.wasPressedThisFrame)
+        {
+            Shoot();
+        }
+        Debug.DrawRay(camera.transform.position, camera.transform.forward, Color.green);
+    }
+
+    void Shoot()
+    {
+        Ray ray = new Ray(camera.transform.position, camera.transform.forward);
+        RaycastHit hit;
+        if (Physics.Raycast(ray, out hit, Mathf.Infinity, enemyLayer))
+        {
+            if (hit.collider.CompareTag("Enemy"))
+            {
+                if (hit.collider.gameObject.name.Contains("Head"))
+                {
+                    Debug.Log("Head");
+                } else if (hit.collider.gameObject.name.Contains("Body"))
+                {
+                    Debug.Log("Body");
+                }
+            } else
+            {
+                Debug.Log("Miss");
             }
         }
     }
